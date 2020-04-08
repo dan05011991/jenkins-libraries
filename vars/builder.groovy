4d6549f698726b0434f826595226f442cbe2117d
Dan Foley <>
2020-04-08 12:49:32 +0100

    test

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -30,8 +30,8 @@ def call(Map pipelineParams) {
                     sh 'git add pom.xml'
                     sh 'git commit -m \'Automated commit: release project\''
 
-                    sshagent(['2f7c1cda-f99d-415d-9cf7-e79b414112fc']) {
-                        sh "git push origin master"
+                    withCredentials([usernamePassword(credentialsId: '2f7c1cda-f99d-415d-9cf7-e79b414112fc', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+                        sh('git push origin master')
                     }
 
                 }
