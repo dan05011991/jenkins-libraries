0e3a31c23c03dbe1adcac35cf5e59df1a1ccf290
Dan Foley <>
2020-04-08 12:13:08 +0100

    test

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -5,14 +5,14 @@ def call(Map pipelineParams) {
         stages {
 
             stage('Build') { // for display purposes
-                steps {
-                    // Get some code from a GitHub repository
-                    git(
-                        branch: 'master',
-                        url: 'https://github.com/dan05011991/demo-application.git',
-                        credentialsId: '2f7c1cda-f99d-415d-9cf7-e79b414112fc'
-                    )
-                }
+//                steps {
+//                    // Get some code from a GitHub repository
+//                    git(
+//                        branch: 'master',
+//                        url: 'https://github.com/dan05011991/demo-application-.git',
+//                        credentialsId: '2f7c1cda-f99d-415d-9cf7-e79b414112fc'
+//                    )
+//                }
 
             }
 
