39114ac47afe29db5a54cc7a8eefa402de8738b7
Dan Foley <>
2020-04-13 20:43:36 +0100

    Updated

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -90,7 +90,7 @@ def call(Map pipelineParams) {
 
                 when {
                     expression {
-                        !isOpsBuild() && !isRefBuild() && !IS_BUMP_COMMIT
+                        !isOpsBuild() && !isRefBuild()
                     }
                 }
 
@@ -175,63 +175,61 @@ def call(Map pipelineParams) {
                 }
             }
 
-            stage('Push changes') {
-                stages {
-                    stage('Push docker update') {
+            stage('Push docker update') {
 
-                        when {
-                            expression {
-                                isRefBuild() && !IS_BUMP_COMMIT
-                            }
-                        }
-
-                        steps {
-                            dir('project') {
-                                script {
-                                    withDockerRegistry([ credentialsId: "dockerhub", url: "" ]) {
-                                        sh "docker push ${pipelineParams.imageName}${DOCKER_TAG_VERSION}"
-                                    }
-                                }
-                            }
-                        }
+                when {
+                    expression {
+                        isRefBuild() && !IS_BUMP_COMMIT
                     }
+                }
 
-                    stage('Push project update') {
-
-                        when {
-                            expression {
-                                isRefBuild() && !IS_BUMP_COMMIT
-                            }
-                        }
-
-                        steps {
-                            dir('project') {
-                                sshagent(credentials: ['ssh']) {
-                                    sh "git push origin ${SOURCE_BRANCH}"
-                                }
-                            }
-                        }
-                    }
-
-                    stage('Push deployment update') {
-
-                        when {
-                            expression {
-                                !IS_BUMP_COMMIT
-                            }
-                        }
-
-                        steps {
-                            dir('deployment') {
-                                sshagent(credentials: ['ssh']) {
-                                    sh "git push origin ${SOURCE_BRANCH}"
-                                }
+                steps {
+                    dir('project') {
+                        script {
+                            withDockerRegistry([ credentialsId: "dockerhub", url: "" ]) {
+                                sh "docker push ${pipelineParams.imageName}${DOCKER_TAG_VERSION}"
                             }
                         }
                     }
                 }
             }
-            
+
+            stage('Push project update') {
+
+                when {
+                    expression {
+                        isRefBuild() && !IS_BUMP_COMMIT
+                    }
+                }
+
+                steps {
+                    dir('project') {
+                        sshagent(credentials: ['ssh']) {
+                            sh "git push origin ${SOURCE_BRANCH}"
+                        }
+                    }
+                }
+            }
+
+            stage('Push deployment update') {
+
+                when {
+                    expression {
+                        !IS_BUMP_COMMIT
+                    }
+                }
+
+                steps {
+                    dir('deployment') {
+                        sshagent(credentials: ['ssh']) {
+                            sh "git push origin ${SOURCE_BRANCH}"
+                        }
+                    }
+                }
+            }
+
+
+
             stage('Deploy to Ref') {
                 agent {
                     label 'ref'
