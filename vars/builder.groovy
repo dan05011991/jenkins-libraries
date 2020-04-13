3480a3aec8038f0f5a5054c17576a68b8aa4186b
Dan Foley <>
2020-04-13 18:18:41 +0100

    Updated

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -38,7 +38,7 @@ def call(Map pipelineParams) {
                 steps {
                     dir('project') {
                         script {
-                            println "$SOURCE"
+                            echo $SOURCE
                         }
 
                         git(
