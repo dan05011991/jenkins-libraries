a0d0dc38b37203954f7c0e50cdeeb16d42150312
Dan Foley <>
2020-04-14 14:11:33 +0100

    Updated

--- vars/builder.groovy
+++ vars/builder.groovy
@@ -105,7 +105,7 @@ def call(Map pipelineParams) {
                 }
 
                 steps {
-                    sh "docker build -f ${pipelineParams.testDockerFile}"
+                    sh "docker build -f ${pipelineParams.testDockerFile} ."
                 }
             }
 
