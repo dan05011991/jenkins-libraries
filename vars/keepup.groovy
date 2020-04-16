cd9fc44b0e563f14f7ab82c600eb63e7538009c3
Dan Foley <>
2020-04-16 14:37:22 +0100

    Test

--- vars/keepup.groovy
+++ vars/keepup.groovy
@@ -15,13 +15,11 @@ pipeline {
             }
 
             steps {
-                stage ('Starting ART job') {
-                    build job: 'Deploy', parameters: [
-                            [$class: 'StringParameterValue', name: 'environment', value: "ops"],
-                            [$class: 'StringParameterValue', name: 'repo', value: "git@github.com:dan05011991/demo-application-docker.git"],
-                            [$class: 'StringParameterValue', name: 'branch', value: "develop"]
-                    ]
-                }
+                build job: 'Deploy', parameters: [
+                        [$class: 'StringParameterValue', name: 'environment', value: "ops"],
+                        [$class: 'StringParameterValue', name: 'repo', value: "git@github.com:dan05011991/demo-application-docker.git"],
+                        [$class: 'StringParameterValue', name: 'branch', value: "develop"]
+                ]
             }
         }
 
@@ -32,13 +30,11 @@ pipeline {
             }
 
             steps {
-                stage ('Starting ART job') {
-                    build job: 'Deploy', parameters: [
-                            [$class: 'StringParameterValue', name: 'environment', value: "ops"],
-                            [$class: 'StringParameterValue', name: 'repo', value: "git@github.com:dan05011991/demo-application-docker.git"],
-                            [$class: 'StringParameterValue', name: 'branch', value: "master"]
-                    ]
-                }
+                build job: 'Deploy', parameters: [
+                        [$class: 'StringParameterValue', name: 'environment', value: "ops"],
+                        [$class: 'StringParameterValue', name: 'repo', value: "git@github.com:dan05011991/demo-application-docker.git"],
+                        [$class: 'StringParameterValue', name: 'branch', value: "master"]
+                ]
             }
         }
     }
