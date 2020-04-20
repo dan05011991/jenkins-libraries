e9f9bcb13e9294b84059c6ad5b793a7446ecc32c
Dan Foley <>
2020-04-20 19:49:09 +0100

    Updated

---
+++ vars/backup.groovy
@@ -0,0 +1,101 @@
+if(isRefBuild()) {
+    def Boolean createRelease = false
+
+    // Input Step
+    timeout(time: 1, unit: "MINUTES") {
+        createRelease = input(
+                message: "Do you want to create a release from this branch",
+                ok: 'Submit',
+                parameters: [
+                        booleanParam(
+                                defaultValue: true,
+                                description: 'This will create a release candidate image',
+                                name: 'Create release branch'
+                        )
+                ],
+                submitter: 'john'
+        )
+        echo("Input : ${createRelease}")
+    }
+
+    if (createRelease) {
+
+        node {
+            properties([
+                    disableConcurrentBuilds()
+            ])
+
+            stage('Create Release') {
+
+                dir('project') {
+                    script {
+                        echo "Commit: ${SOURCE_BRANCH}"
+                        echo "Docker: ${DOCKER_TAG_VERSION}"
+                    }
+
+                    git(
+                            branch: "${SOURCE_BRANCH}",
+                            url: "${SOURCE_URL}",
+                            credentialsId: 'ssh'
+                    )
+
+                    sh "git checkout ${SOURCE_COMMIT}"
+
+                    sh "git checkout -b release/release-${DOCKER_TAG_VERSION}"
+                    sh "git push origin release/release-${DOCKER_TAG_VERSION}"
+                }
+            }
+        }
+    }
+}
+
+def deploy = input(
+        message: "Do you want to deploy ${DOCKER_TAG_VERSION}",
+        ok: 'Submit',
+        parameters: [
+                booleanParam(
+                        defaultValue: false,
+                        description: 'This will update the deployment file and deploy the image',
+                        name: 'Deploy image'
+                )
+        ],
+        submitter: 'john'
+)
+echo("Input : ${deploy}")
+
+if (deploy) {
+
+    node {
+        properties([
+                disableConcurrentBuilds()
+        ])
+
+        stage('Deploy') {
+
+            customParallel([
+                    step('Deploy to Ref', isRefBuild(), {
+
+                        dir('deployment') {
+
+                            build job: 'Deploy', parameters: [
+                                    [$class: 'StringParameterValue', name: 'environment', value: "ref"],
+                                    [$class: 'StringParameterValue', name: 'repo', value: "${pipelineParams.deploymentRepo}"],
+                                    [$class: 'StringParameterValue', name: 'branch', value: "${SOURCE_BRANCH}"]
+                            ]
+                        }
+                    }),
+                    step('Deploy to Ops', isOpsBuild(), {
+
+                        dir('deployment') {
+
+                            build job: 'Deploy', parameters: [
+                                    [$class: 'StringParameterValue', name: 'environment', value: "ops"],
+                                    [$class: 'StringParameterValue', name: 'repo', value: "${pipelineParams.deploymentRepo}"],
+                                    [$class: 'StringParameterValue', name: 'branch', value: "${SOURCE_BRANCH}"]
+                            ]
+                        }
+                    })
+            ])
+        }
+    }
+}
--- vars/builder.groovy
+++ vars/builder.groovy
@@ -5,8 +5,11 @@ def call(Map pipelineParams) {
     def String SOURCE_COMMIT = ''
     def String SOURCE_BRANCH = "${BRANCH_NAME}"
     def String SOURCE_URL = "${scm.userRemoteConfigs[0].url}"
+
     def String IS_BUMP_COMMIT = false
     def String DOCKER_TAG_VERSION = 'EXAMPLE'
+    def String PROJECT_VERSION = 'EXAMPLE'
+
     def String PROJECT_DIR = ''
     def String DEPLOYMENT_DIR = ''
 
@@ -35,7 +38,7 @@ def call(Map pipelineParams) {
                             }
                         }
                     }),
-                    step('Checkout Deployment', isOpsBuild() || isRefBuild(), {
+                    step('Checkout Deployment', isSpecialBuild(), {
 
                         dir('deployment') {
                             git(
@@ -61,7 +64,7 @@ def call(Map pipelineParams) {
             ])
         }
 
-        stage('Is Bump Commit?', isOpsBuild() && isRefBuild(), {
+        stage('Is Bump Commit?', isSpecialBuild(), {
 
             echo "This is a bump commit build - exiting early"
 
@@ -116,16 +119,16 @@ def call(Map pipelineParams) {
             ])
         })
 
-        stage('Update project version', isRefBuild() && !IS_BUMP_COMMIT, {
+        stage('Update project version', isSpecialBuild() && !IS_BUMP_COMMIT, {
 
             customParallel([
-
                     stage('Maven', pipelineParams.buildType == 'maven', {
 
                         dir('project') {
-                            sh 'mvn release:update-versions -B'
+                            sh 'mvn versions:set -DremoveSnapshot'
                             sh 'git add pom.xml'
                             sh 'git commit -m "[Automated commit: version bump]"'
+
                         }
                     }),
                     stage('Gulp', pipelineParams.buildType == 'gulp', {
@@ -139,15 +142,15 @@ def call(Map pipelineParams) {
                                         returnStdout: true
                                 )
 
-                                DOCKER_TAG_VERSION = sh(
+                                PROJECT_VERSION = sh(
                                         script: "./increment_version.sh ${UI_VERSION}",
                                         returnStdout: true
                                 ).trim()
 
                                 sh("""
-                                #!/bin/bash
-                                sed -i "s/appVersion: '${UI_VERSION}'/appVersion: '${DOCKER_TAG_VERSION}'/g" conf/config-release.js
-                            """)
+                                    #!/bin/bash
+                                    sed -i "s/appVersion: '${UI_VERSION}'/appVersion: '${PROJECT_VERSION}'/g" conf/config-release.js
+                                """)
 
                                 sh 'git add conf/config-release.js'
                                 sh 'git commit -m "[Automated commit: version bump]"'
@@ -157,7 +160,7 @@ def call(Map pipelineParams) {
             ])
         })
 
-        stage('Get deployment version', isOpsBuild() || isRefBuild(), {
+        stage('Get deployment version', isSpecialBuild(), {
 
             customParallel([
 
@@ -165,7 +168,7 @@ def call(Map pipelineParams) {
 
                         dir('project') {
                             script {
-                                DOCKER_TAG_VERSION = sh(
+                                PROJECT_VERSION = sh(
                                         script: 'mvn -q -Dexec.executable=echo -Dexec.args=\'${project.version}\' --non-recursive exec:exec',
                                         returnStdout: true
                                 ).trim()
@@ -177,7 +180,7 @@ def call(Map pipelineParams) {
                         script {
 
                             dir('project') {
-                                DOCKER_TAG_VERSION = sh(
+                                PROJECT_VERSION = sh(
                                         script: "sed -n \"s/^.*appVersion.*'\\(.*\\)'.*\$/\\1/ p\" conf/config-release.js | tr -d '\\n'",
                                         returnStdout: true
                                 )
@@ -187,17 +190,33 @@ def call(Map pipelineParams) {
             ])
         })
 
-        stage('Docker build and tag', isRefBuild() && !IS_BUMP_COMMIT, {
+        stage('Docker build and tag', (isRefBuild() || isReleaseBuild()) && !IS_BUMP_COMMIT, {
 
             dir('project') {
 
                 script {
+                    DOCKER_TAG_VERSION = getDockerTag(PROJECT_VERSION)
+                    sh "git tag -a ${DOCKER_TAG_VERSION} -m \"Release tagged\""
                     sh "docker build . -t ${pipelineParams.imageName}${DOCKER_TAG_VERSION}"
                 }
             }
         })
 
-        stage('Push Project Updates', isRefBuild() && !IS_BUMP_COMMIT, {
+        stage('Prepare project for next iteration', isSpecialBuild() && !IS_BUMP_COMMIT, {
+            customParallel([
+
+                    stage('Maven', pipelineParams.buildType == 'maven', {
+
+                        dir('project') {
+                            sh 'mvn release:update-versions -B'
+                            sh 'git add pom.xml'
+                            sh 'git commit -m "[Automated commit: version bump]"'
+                        }
+                    })
+            ])
+        })
+
+        stage('Push Project Updates', isSpecialBuild() && !IS_BUMP_COMMIT, {
 
             customParallel([
                     step('Push docker image') {
@@ -215,20 +234,23 @@ def call(Map pipelineParams) {
                         dir('project') {
                             sshagent(credentials: ['ssh']) {
                                 sh "git push origin ${SOURCE_BRANCH}"
+                                sh "git push origin ${DOCKER_TAG_VERSION}"
                             }
                         }
                     }
             ])
         })
+    }
 
-        stage('Update compose version', isOpsBuild() || isRefBuild(), {
 
-            stage('Update file') {
+    stage('Update compose version', isSpecialBuild(), {
 
-                dir('deployment') {
+        stage('Update file') {
 
-                    sshagent(credentials: ['ssh']) {
-                        sh """
+            dir('deployment') {
+
+                sshagent(credentials: ['ssh']) {
+                    sh """
                             IMAGE=\$(echo ${pipelineParams.imageName} | sed 's/\\//\\\\\\//g')
                             COMPOSE_FILE=docker-compose.yaml
                             SNAPSHOT=${DOCKER_TAG_VERSION}
@@ -241,121 +263,27 @@ def call(Map pipelineParams) {
                             fi
             
                         """
-                    }
-                }
-            }
-
-            stage('Push File') {
-                dir('deployment') {
-                    sshagent(credentials: ['ssh']) {
-                        sh "git push origin ${SOURCE_BRANCH}"
-                    }
-                }
-            }
-        })
-    }
-
-
-    if(isRefBuild()) {
-        def Boolean createRelease = false
-
-        // Input Step
-        timeout(time: 15, unit: "MINUTES") {
-            createRelease = input(
-                    message: "Do you want to create a release from this branch",
-                    ok: 'Submit',
-                    parameters: [
-                            booleanParam(
-                                    defaultValue: true,
-                                    description: 'This will create a release candidate image',
-                                    name: 'Create release branch'
-                            )
-                    ],
-                    submitter: 'john'
-            )
-            echo("Input : ${createRelease}")
-        }
-
-        if (createRelease) {
-
-            node {
-                properties([
-                        disableConcurrentBuilds()
-                ])
-
-                stage('Create Release') {
-
-                    dir('project') {
-                        script {
-                            echo "Commit: ${SOURCE_BRANCH}"
-                            echo "Docker: ${DOCKER_TAG_VERSION}"
-                        }
-
-                        git(
-                                branch: "${SOURCE_BRANCH}",
-                                url: "${SOURCE_URL}",
-                                credentialsId: 'ssh'
-                        )
-
-                        sh "git checkout ${SOURCE_COMMIT}"
-
-                        sh "git checkout -b release/release-${DOCKER_TAG_VERSION}"
-                        sh "git push origin release/release-${DOCKER_TAG_VERSION}"
-                    }
                 }
             }
         }
-    }
 
-    def deploy = input(
-            message: "Do you want to deploy ${DOCKER_TAG_VERSION}",
-            ok: 'Submit',
-            parameters: [
-                    booleanParam(
-                            defaultValue: false,
-                            description: 'This will update the deployment file and deploy the image',
-                            name: 'Deploy image'
-                    )
-            ],
-            submitter: 'john'
-    )
-    echo("Input : ${deploy}")
-
-    if (deploy) {
-
-        node {
-            properties([
-                    disableConcurrentBuilds()
-            ])
-
-            stage('Deploy') {
-
-                customParallel([
-                        step('Deploy to Ref', isRefBuild(), {
-
-                            dir('deployment') {
-
-                                build job: 'Deploy', parameters: [
-                                        [$class: 'StringParameterValue', name: 'environment', value: "ref"],
-                                        [$class: 'StringParameterValue', name: 'repo', value: "${pipelineParams.deploymentRepo}"],
-                                        [$class: 'StringParameterValue', name: 'branch', value: "${SOURCE_BRANCH}"]
-                                ]
-                            }
-                        }),
-                        step('Deploy to Ops', isOpsBuild(), {
-
-                            dir('deployment') {
-
-                                build job: 'Deploy', parameters: [
-                                        [$class: 'StringParameterValue', name: 'environment', value: "ops"],
-                                        [$class: 'StringParameterValue', name: 'repo', value: "${pipelineParams.deploymentRepo}"],
-                                        [$class: 'StringParameterValue', name: 'branch', value: "${SOURCE_BRANCH}"]
-                                ]
-                            }
-                        })
-                ])
+        stage('Push File') {
+            dir('deployment') {
+                sshagent(credentials: ['ssh']) {
+                    sh "git push origin ${SOURCE_BRANCH}"
+                }
             }
         }
+    })
+}
+
+def getDockerTag(version) {
+    if(isOpsBuild()) {
+        return version
+    } else if(isRefBuild()) {
+        return version + '-SNAPSHOT'
+    } else if(isReleaseBuild()) {
+        return version + '-release-candidate'
     }
 }
 
@@ -366,6 +294,10 @@ def lastCommitIsBumpCommit() {
     } else {
         return false
     }
+}
+
+def isSpecialBuild() {
+    return isRefBuild() || isOpsBuild() || isReleaseBuild()
 }
 
 def isRefBuild() {
